8b78154aeb098385ae2bae79a693def020a885ea
Daniel <daniel.alvarez.vaquero@gmail.com>
2014-11-22 02:49:34 +0100

    Modificado el metodo updateDisplay para que muestre hora+fecha y modificado el metodo setTime para poder fijar hora y fecha

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -10,53 +10,74 @@ public class ClockDisplay
     // definimos los atributos de la clase ColckDisplay
     private NumberDisplay horas;
     private NumberDisplay minutos;
+    private NumberDisplay dia;
+    private NumberDisplay mes;
+    private NumberDisplay año;
     private String horaActual;
-   
-    
+    private String fechaActual;
     /**
-     * Constructor para fijar las horas y minutos a cero
+     * Constructor para fijar las horas y minutos, dia, mes y año a cero de un objeto
      */
     public ClockDisplay()
     {
-      horas = new NumberDisplay (24);
+      horas = new NumberDisplay (24); 
       minutos = new NumberDisplay (60);
-      updateDisplay();
+      dia = new NumberDisplay (30); 
+      mes = new NumberDisplay (12);
+      año = new NumberDisplay (99);
+      updateDisplay(); 
+      
       
     }
    
     /**
-     * Constructor de la clase ClockDisplay para fijar las horas y minutos
+     * Constructor de la clase ClockDisplay para fijar las horas, los minutos, 
+     * el dia, el mes y el año en una cifra determinada de un objeto
      */
-    public ClockDisplay(int nuevaHora, int nuevosMinutos)
+    public ClockDisplay(int nuevaHora, int nuevosMinutos, int nuevoDia, int nuevoMes, int nuevoAño) 
+                                                                                                  
     {
-        // initialise instance variables
-        horas = new NumberDisplay (24);
+        horas = new NumberDisplay (24); 
         minutos = new NumberDisplay (60);
-        horas.setValue(nuevaHora);
+        horas.setValue(nuevaHora); 
         minutos.setValue(nuevosMinutos);
+        dia = new NumberDisplay (30);
+        mes = new NumberDisplay (12);
+        año = new NumberDisplay (99);
+        dia.setValue(nuevoDia);
+        mes.setValue(nuevoMes);
+        año.setValue(nuevoAño);
         updateDisplay();
     }
-    
+   
     /**
-     * Metodo que fija las horas y minutos en los valores de la hora actual
+     * Metodo que fija las horas, minutos, dia, mes y año en los valores deseados, cuando no se han fijado por parametro
    
      */
-    public void setTime (int actualHora, int actualMinutos)    
+    public void setTime (int actualHora, int actualMinutos, int actualDia, int actualMes, int actualAño)    
     {
-       horas.setValue(actualHora);
+       horas.setValue(actualHora); 
        minutos.setValue(actualMinutos);
-       updateDisplay();
+       dia.setValue(actualDia);
+       mes.setValue(actualMes);
+       año.setValue(actualAño);
+       updateDisplay(); //¿?
+    }
+    
+    public void setFecha (int actualDia, int actualMes, int actualAño)
+    {
+        dia.setValue(actualDia);
+        mes.setValue(actualMes);
+        año.setValue(actualAño);
+
     }
     
     /**
-     * Método que devuelve la hora en formato de 5 caracteres
+     * Método que devuelve la hora en formato de 16 caracteres
      */
     public String getTime ()
     {
-        updateDisplay(); //esta linea
-        //se podria omitir ya que el valor horaActual, se mantiene actualizado siempre qeu
-        //se utilizan los metodos anteriores.
-        return horaActual;
+        return horaActual + " " +  fechaActual;
     }   
     
     /**
@@ -72,32 +93,81 @@ public class ClockDisplay
             horas.increment();
         }
         
-        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        if (horas.getValue() > 24)
+        {
+            dia.increment();
+        }
         
+        if (dia.getValue() > 30)
+        {
+            mes.increment();
+        }
+        
+        if (mes.getValue() > 12)
+        {
+            año.increment();
+        }
+        
+        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + "a.m";
+        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + "p.m";
+        fechaActual = dia.getDisplayValue() + "/" + mes.getDisplayValue() + "/" + año.getDisplayValue();
+     
     }
     
     /**
-     * Actualiza el atributo display
+     * Metodo que actualiza el atributo display
      */
     private void updateDisplay()
+    
+    
     {
         if (horas.getValue() < 12)
         {
-             horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " a.m";
+             
+            if (horas.getValue() == 0)
+             {
+
+                 horaActual = "12" + ":" + minutos.getDisplayValue() + " p.m";
+                 fechaActual = dia.getDisplayValue() + "/" + mes.getDisplayValue() + "/" + año.getDisplayValue();
+               
+             }
+             
+             else
+             
+             {
+                
+                 horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " a.m";
+                 fechaActual = dia.getDisplayValue() + "/" + mes.getDisplayValue() + "/" + año.getDisplayValue();
+           
+             }
         }
+        
         else 
         {
             int horapm = (horas.getValue() - 12);
-                if (horapm < 10)
-                {
-                    horaActual = "0" + horapm + ":" + minutos.getDisplayValue() + " p.m";
-                }
-            else
-                {
-                     horaActual = horapm + ":" + minutos.getDisplayValue() + " p.m";
-                }
+            
+            if ((horapm < 10) && (horapm == 0))
+            {
+                horaActual = "0" + horapm + ":" + minutos.getDisplayValue() + " p.m";
+                fechaActual = dia.getDisplayValue() + "/" + mes.getDisplayValue() + "/" + año.getDisplayValue();
+                
+            }
+            else if (horapm == 0)
+            {
+                horaActual = "12" + ":" + minutos.getDisplayValue() + " p.m";
+                fechaActual = dia.getDisplayValue() + "/" + mes.getDisplayValue() + "/" + año.getDisplayValue();
+
+            }
            
+            else
+            {
+                horaActual = horapm + ":" + minutos.getDisplayValue() + " p.m";
+                fechaActual = dia.getDisplayValue() + "/" + mes.getDisplayValue() + "/" + año.getDisplayValue();
+
+            }
+
         }
+       
     }
 }
        
